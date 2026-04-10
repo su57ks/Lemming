5eadb11b78f2d31870738b70eafb3d34ac5bd78d
su57ks <samuray.ks@yandex.ru>
2026-04-10 18:18:36 +0300

    very bad

--- Python/parser.py
+++ Python/parser.py
@@ -1,1 +1,12 @@
+import re 
 
+class Parser():
+    def __init__(self, pText):
+        self.text = pText
+        self.position = 0
+        self.programm = []
+
+    def parse(self):
+        for string in self.text:
+            if re.search(r"print(.)", string):
+                pass
